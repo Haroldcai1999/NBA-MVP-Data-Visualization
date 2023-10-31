01465380c9ffe085021a72f62ae5a314790606d6
Zining Cai <143218173+Haroldcai1999@users.noreply.github.com>
2023-10-31 06:22:10 -0700

    Add files via upload
    NBA Player Name Update

--- sketch.js
+++ sketch.js
@@ -6,21 +6,22 @@ let years = [];
 let selectedWinner = null;
 
 function preload() {
-
-  data = loadJSON('NBA MVP.json'); 
+  data = loadJSON('NBA MVP.json');
 }
 
 function setup() {
   background(220);
-    let canvas = createCanvas(800, 400);
-    canvas.parent('data');
+  let canvas = createCanvas(800, 400);
+  canvas.parent('data');
 
   for (let year in data.winners) {
     const winner = data.winners[year];
+    const winnerData = { year: int(year), name: winner.name, clicked: false }; 
+
     if (winner.coast === 'West') {
-      westCoastWinners.push({ year: int(year), name: winner.name });
+      westCoastWinners.push(winnerData);
     } else if (winner.coast === 'East') {
-      eastCoastWinners.push({ year: int(year), name: winner.name });
+      eastCoastWinners.push(winnerData);
     }
     years.push(int(year));
   }
@@ -28,9 +29,9 @@ function setup() {
   westCoastWinners.sort((a, b) => a.year - b.year);
   eastCoastWinners.sort((a, b) => a.year - b.year);
 
-  drawWinners(westCoastWinners, height * 0.25, color(255, 0, 0)); 
+  drawWinners(westCoastWinners, height * 0.25, color(255, 0, 0));
 
-  drawWinners(eastCoastWinners, height * 0.75, color(0, 0, 255)); 
+  drawWinners(eastCoastWinners, height * 0.75, color(0, 0, 255));
 
   drawTimeline(min(years), max(years), height / 2);
 }
@@ -38,14 +39,19 @@ function setup() {
 function drawWinners(winners, yPos, winnerColor) {
   const spacing = 20;
   for (let i = 0; i < winners.length; i++) {
-    fill(winnerColor);
-    noStroke();
     const x = map(winners[i].year, min(years), max(years), 50, width - 50);
     const d = dist(mouseX, mouseY, x, yPos);
-    if (d < 10) {
-      fill(255, 0, 0); 
+
+    if (winners[i].clicked) {
+      fill(winnerColor.levels[0], winnerColor.levels[1], winnerColor.levels[2], 128);
+    } else if (d < 10) {
+      fill(255, 0, 0);
       selectedWinner = winners[i].name;
+    } else {
+      fill(winnerColor);
     }
+
+    noStroke();
     ellipse(x, yPos, 10, 10);
   }
 }
@@ -65,11 +71,36 @@ function drawTimeline(startYear, endYear, yPos) {
   }
 }
 
+function mousePressed() {
+  for (let winner of westCoastWinners) {
+    const x = map(winner.year, min(years), max(years), 50, width - 50);
+    const d = dist(mouseX, mouseY, x, height * 0.25);
+    if (d < 10) {
+      winner.clicked = !winner.clicked;
+      return;
+    }
+  }
+  
+  for (let winner of eastCoastWinners) {
+    const x = map(winner.year, min(years), max(years), 50, width - 50);
+    const d = dist(mouseX, mouseY, x, height * 0.75);
+    if (d < 10) {
+      winner.clicked = !winner.clicked;
+      return;
+    }
+  }
+}
+
 function mouseMoved() {
   selectedWinner = null;
 }
 
 function draw() {
+  background(220); 
+  drawWinners(westCoastWinners, height * 0.25, color(255, 0, 0));
+  drawWinners(eastCoastWinners, height * 0.75, color(0, 0, 255));
+  drawTimeline(min(years), max(years), height / 2);
+  
   if (selectedWinner !== null) {
     textSize(16);
     fill(0);
